cef8ee79f3e4a28f0bdd1e7dc8a5b504971c0304
LastVision <daniel.carlsson92@gmail.com>
2015-11-24 14:27:08 +0100

    Added PostMaster with only base message and resizeMessage

---
+++ Solution/PostMaster/Subscriber.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "Subscriber.h"
+#include "Message.h"
+
+Subscriber::Subscriber()
+{
+}
+
+
+Subscriber::~Subscriber()
+{
+}
+
+void Subscriber::ReceiveMessage(const Message&)
+{
+}
+
+void Subscriber::ReceiveMessage(const ResizeMessage&)
+{
+}
