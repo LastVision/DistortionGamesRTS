b573e048f184bc8db171917c11344edafe77ac42
fabs <fabian.ahlund.sp14@thegameassembly.com>
2015-11-26 10:08:18 +0100

    moved class

--- Solution/Engine/DebugDrawer.h
+++ Solution/Engine/DebugDrawer.h
@@ -1,8 +1,59 @@
 #pragma once
-class DebugDrawer
-{
-public:
-	DebugDrawer();
-	~DebugDrawer();
-};
 
+#include "Line3D.h"
+#include <GrowingArray.h>
+
+#define WHITE_DEBUG CU::Vector4<float>(1.f, 1.f, 1.f, 1.f)
+#define BLACK_DEBUG CU::Vector4<float>(0.f, 0.f, 0.f, 1.f)
+#define RED_DEBUG CU::Vector4<float>(1.f, 0.f, 0.f, 1.f)
+#define GREEN_DEBUG CU::Vector4<float>(0.f, 1.f, 0.f, 1.f)
+#define BLUE_DEBUG CU::Vector4<float>(0.f, 0.f, 1.f, 1.f)
+#define PINK_DEBUG CU::Vector4<float>(1.f, 0.f, 1.f, 1.f)
+#define YELLOW_DEBUG CU::Vector4<float>(1.f, 1.f, 0.f, 1.f)
+
+namespace Prism
+{
+	class Effect;
+
+	struct VertexBufferWrapper;
+	struct IndexBufferWrapper;
+	class DebugDrawer
+	{
+	public:
+		static DebugDrawer* GetInstance();
+		static void Destroy();
+
+		void RenderLine2D(const CU::Vector2<float>& aFirstPoint, const CU::Vector2<float> aSecondPoint
+			, const CU::Vector4<float>& aColor = PINK_DEBUG);
+		void RenderLine3D(const CU::Vector3<float>& aFirstPoint, const CU::Vector3<float> aSecondPoint
+			, const CU::Vector4<float>& aColor = PINK_DEBUG);
+		void RenderArrow2D(const CU::Vector2<float>& aFirstPoint, const CU::Vector2<float> aSecondPoint
+			, const CU::Vector4<float>& aColor = PINK_DEBUG);
+		void RenderArrow3D(const CU::Vector3<float>& aFirstPoint, const CU::Vector3<float>& aSecondPoint
+			, const CU::Vector4<float>& aColor = PINK_DEBUG);
+		void RenderBox(const CU::Vector3<float>& aPosition, float aSize, const CU::Vector4<float>& aColor = PINK_DEBUG
+			, bool aWireFrame = false);
+		void RenderSphere(const CU::Vector3<float>& aPosition, float aSize, const CU::Vector4<float>& aColor = PINK_DEBUG
+			, bool aWireFrame = false);
+		void RenderText2D(const std::string& aText, const CU::Vector2<float>& aPosition
+			, const CU::Vector4<float>& aColor = PINK_DEBUG);
+		void RenderText3D(const std::string& aText, const CU::Vector3<float>& aPosition
+			, const CU::Vector4<float>& aColor = PINK_DEBUG);
+
+		void Render();
+
+	private:
+		static DebugDrawer* myInstance;
+		DebugDrawer();
+		~DebugDrawer();
+
+		VertexBufferWrapper* myLine3DVertexBuffer;
+		IndexBufferWrapper* myLine3DIndexBuffer;
+		D3D11_PRIMITIVE_TOPOLOGY myPrimitiveTopology;
+
+		Effect* myEffect;
+
+		CU::GrowingArray<Line3D> my3DLines;
+
+	};
+}
