b1736a247b07779b1cc39a6b6db4df4003fbef8c
fabs <fabian.ahlund.sp14@thegameassembly.com>
2015-11-27 14:42:40 +0100

    added collisiontempcode for units and made a constructor in linesegment 3d

--- Solution/CommonUtilities/LineSegment3D.h
+++ Solution/CommonUtilities/LineSegment3D.h
@@ -7,6 +7,10 @@ namespace CU
 		class LineSegment3D
 		{
 		public:
+			LineSegment3D(const CU::Vector3<float>& aStartPos, const CU::Vector3<float>& aEndPos)
+				: myStartPos(aStartPos)
+				, myEndPos(aEndPos)
+			{}
 			CU::Vector3<float> myStartPos;
 			CU::Vector3<float> myEndPos;
 		};
