5236ac8f7d45c6ffba7908e4a0237b951caa65c5
nian0601 <anderssonniklas@outlook.com>
2015-11-27 12:46:30 +0100

    Implemented MovementComponent

--- Solution/Entity/MovementComponent.cpp
+++ Solution/Entity/MovementComponent.cpp
@@ -1,11 +1,16 @@
 #include "stdafx.h"
 #include "Entity.h"
 #include "MovementComponent.h"
+#include <Terrain.h>
 #include <XMLReader.h>
 
-MovementComponent::MovementComponent(Entity& aEntity, float aMovementSpeed)
+MovementComponent::MovementComponent(Entity& aEntity, float aMovementSpeed, Prism::Terrain& aTerrain)
 	: Component(aEntity)
 	, myMovementSpeed(aMovementSpeed)
+	, myTargetPosition(myEntity.myOrientation.GetPos())
+	, myWayPoints(8)
+	, myCurrentWayPoint(0)
+	, myTerrain(aTerrain)
 {
 }
 
@@ -15,9 +20,66 @@ MovementComponent::~MovementComponent()
 
 void MovementComponent::Update(float aDeltaTime)
 {
+	if (myEntity.GetState() == eEntityState::IDLE && myWayPoints.Size() > 0)
+	{
+		MoveTo(myWayPoints[myCurrentWayPoint]);
+		++myCurrentWayPoint;
+		if (myCurrentWayPoint >= myWayPoints.Size())
+		{
+			myWayPoints.RemoveAll();
+			myCurrentWayPoint = 0;
+		}
+	}
+
+
+	CU::Vector3<float> position = myEntity.myOrientation.GetPos();
+	myEntity.myOrientation.SetPos({ position.x, 0.f, position.z });
+
+	float dotResult = CU::Dot(myEntity.myOrientation.GetForward(), myTargetPosition - myEntity.myOrientation.GetPos());
+	if (dotResult > 0)
+	{
+		CU::Vector3<float> newPos = myEntity.myOrientation.GetPos();
+		newPos += myDirection * myMovementSpeed * aDeltaTime;
+		myEntity.myOrientation.SetPos(newPos);
+	}
+	else
+	{
+		myEntity.myOrientation.SetPos(myTargetPosition);
+		myDirection = { 0.f, 0.f, 0.f };
+		myEntity.SetState(eEntityState::IDLE);
+	}
+
+	myTerrain.CalcEntityHeight(myEntity.myOrientation);
 }
 
-void MovementComponent::MoveTo(CU::Vector2<float> aPointToMoveTo)
+void MovementComponent::SetWayPoints(const CU::GrowingArray<CU::Vector3<float>>& someWayPoints)
 {
-	myEntity.myOrientation.SetPos({ aPointToMoveTo.x, 0, aPointToMoveTo.y });
+	myWayPoints.RemoveAll();
+	for (int i = 0; i < someWayPoints.Size(); ++i)
+	{
+		myWayPoints.Add(someWayPoints[i]);
+	}
+
+	myCurrentWayPoint = 0;
+}
+
+void MovementComponent::MoveTo(CU::Vector3<float> aPointToMoveTo)
+{
+	myTargetPosition = { aPointToMoveTo };
+	myDirection = CU::Vector3<float>(aPointToMoveTo) -myEntity.myOrientation.GetPos();
+	CU::Normalize(myDirection);
+
+	CU::Vector3<float> forward = myEntity.myOrientation.GetForward();
+	CU::Normalize(forward);
+
+	if (forward != myDirection)
+	{
+		float angle = acos(CU::Dot(forward, myDirection));
+		CU::Vector3<float> pos = myEntity.myOrientation.GetPos();
+		myEntity.myOrientation.SetPos({ 0.f, 0.f, 0.f });
+		myEntity.myOrientation = myEntity.myOrientation * CU::Matrix44<float>::CreateRotateAroundY(angle);
+		myEntity.myOrientation.SetPos(pos);
+	}
+
+	myEntity.SetState(eEntityState::WALKING);
 }
--- Solution/Entity/MovementComponent.h
+++ Solution/Entity/MovementComponent.h
@@ -1,25 +1,42 @@
 #pragma once
 #include "Component.h"
 #include <Vector.h>
+#include <GrowingArray.h>
+
+namespace Prism
+{
+	class Terrain;
+}
+
 class MovementComponent : public Component
 {
 public:
-	MovementComponent(Entity& aEntity, float aMovementSpeed);
+	MovementComponent(Entity& aEntity, float aMovementSpeed, Prism::Terrain& aTerrain);
 	~MovementComponent();
 
-	void MoveTo(CU::Vector2<float> aPointToMoveTo);
-
 	void Update(float aDeltaTime) override;
+	void SetWayPoints(const CU::GrowingArray<CU::Vector3<float>>& someWayPoints);
 
 	static eComponentType GetTypeStatic();
+	eComponentType GetType() override;
 
 private:
+	void MoveTo(CU::Vector3<float> aPointToMoveTo);
 
 	float myMovementSpeed;
-
+	CU::Vector3<float> myTargetPosition;
+	CU::Vector3<float> myDirection;
+	CU::GrowingArray<CU::Vector3<float>> myWayPoints;
+	int myCurrentWayPoint;
+	Prism::Terrain& myTerrain;
 };
 
 inline eComponentType MovementComponent::GetTypeStatic()
 {
 	return eComponentType::MOVEMENT;
+}
+
+inline eComponentType MovementComponent::GetType()
+{
+	return GetTypeStatic();
 }
