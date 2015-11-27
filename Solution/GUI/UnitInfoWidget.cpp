a807d701fa8926b264f19fe5e8aac23a3a77ca81
apasitteribuske <malin.sandgren.sp14@thegameassembly.com>
2015-11-27 16:53:41 +0100

    Added empty GUI class

---
+++ Solution/GUI/UnitInfoWidget.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "UnitInfoWidget.h"
+
+UnitInfoWidget::UnitInfoWidget()
+{
+}
+
+UnitInfoWidget::~UnitInfoWidget()
+{
+}
---
+++ Solution/GUI/UnitInfoWidget.h
@@ -0,0 +1,8 @@
+#pragma once
+class UnitInfoWidget
+{
+public:
+	UnitInfoWidget();
+	~UnitInfoWidget();
+};
+
