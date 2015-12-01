aea804718ae427b6124b91fad036b2e7f73256ea
apasitteribuske <malin.sandgren.sp14@thegameassembly.com>
2015-12-01 16:58:17 +0100

    Added unit actions widget

--- Solution/GUI/UnitActionWidget.cpp
+++ Solution/GUI/UnitActionWidget.cpp
@@ -1,9 +1,10 @@
 #include "stdafx.h"
 #include "ButtonWidget.h"
-#include "UnitActionWidget.h"
+#include "../Entity/Entity.h"
 #include <Sprite.h>
-#include <XMLReader.h>
+#include "UnitActionWidget.h"
 #include "WidgetContainer.h"
+#include <XMLReader.h>
 
 namespace GUI
 {
@@ -20,31 +21,28 @@ namespace GUI
 		aReader->ForceReadAttribute(aReader->ForceFindFirstChild(anXMLElement, "size"), "y", size.y);
 		aReader->ForceReadAttribute(aReader->ForceFindFirstChild(anXMLElement, "position"), "x", position.x);
 		aReader->ForceReadAttribute(aReader->ForceFindFirstChild(anXMLElement, "position"), "y", position.y);
-		aReader->ForceReadAttribute(aReader->ForceFindFirstChild(anXMLElement, "backgroundsprite"), "path", path);
 
 		mySize = size;
 		myPosition = position;
 
-		tinyxml2::XMLElement* containerElement = aReader->ForceFindFirstChild(anXMLElement, "container");
-		//for (; containerElement != nullptr; containerElement = aReader->FindNextElement(containerElement))
-		//{
-			GUI::WidgetContainer* container = new WidgetContainer(nullptr, size);
+		tinyxml2::XMLElement* containerElement = aReader->ForceFindFirstChild(anXMLElement, "unit");
+		GUI::WidgetContainer* container = new WidgetContainer(nullptr, size);
+		tinyxml2::XMLElement* widgetElement = aReader->FindFirstChild(containerElement, "widget");
+		for (; widgetElement != nullptr; widgetElement = aReader->FindNextElement(widgetElement))
+		{
+			std::string type = "";
 
-			tinyxml2::XMLElement* widgetElement = aReader->FindFirstChild(containerElement, "widget");
-			for (; widgetElement != nullptr; widgetElement = aReader->FindNextElement(widgetElement))
+			aReader->ForceReadAttribute(widgetElement, "type", type);
+
+			if (type == "button")
 			{
-				std::string type = "";
-
-				aReader->ForceReadAttribute(widgetElement, "type", type);
-
-				if (type == "button")
-				{
-					ButtonWidget* button = new ButtonWidget(aReader, widgetElement);
-					container->AddWidget(button);
-				}
+				ButtonWidget* button = new ButtonWidget(aReader, widgetElement);
+				container->AddWidget(button);
 			}
-			myUnitActionButtons = container;
-		//}
+		}
+		myUnitActionButtons = container;
+
+		// load in building actions
 	}
 
 	UnitActionWidget::~UnitActionWidget()
@@ -55,11 +53,24 @@ namespace GUI
 
 	void UnitActionWidget::Render(const CU::Vector2<float>& aParentPosition)
 	{
-		myUnitActionButtons->Render(myPosition + aParentPosition);
+		if (myUnits[0] != nullptr && myUnits[0]->IsSelected() == true)
+		{
+			myUnitActionButtons->Render(myPosition + aParentPosition);
+		}
+	}
+
+	Widget* UnitActionWidget::MouseIsOver(const CU::Vector2<float>& aPosition)
+	{
+		if (myUnits[0] != nullptr && myUnits[0]->IsSelected() == true)
+		{
+			return myUnitActionButtons->MouseIsOver(aPosition - myPosition);
+		}
+		return nullptr;
 	}
 
 	void UnitActionWidget::OnResize(const CU::Vector2<float>& aNewWindowSize, const CU::Vector2<float>& anOldWindowSize)
 	{
+		Widget::OnResize(aNewWindowSize, anOldWindowSize);
 		myUnitActionButtons->OnResize(aNewWindowSize, anOldWindowSize);
 	}
 }
