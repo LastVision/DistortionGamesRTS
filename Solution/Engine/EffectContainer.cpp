6892ae703b69ecfb453e95e56b3633a31b43a9cc
fabs <fabian.ahlund.sp14@thegameassembly.com>
2015-12-01 16:12:21 +0100

    added animated pbl to filtereffect

--- Solution/Engine/EffectContainer.cpp
+++ Solution/Engine/EffectContainer.cpp
@@ -105,6 +105,7 @@ namespace Prism
 	void EffectContainer::VerifyShader(const std::string& aFilePath)
 	{
 		DL_ASSERT_EXP(aFilePath == "Data/Resource/Shader/S_effect_pbl.fx"
+			|| aFilePath == "Data/Resource/Shader/S_effect_pbl_animated.fx"
 			|| aFilePath == "Data/Resource/Shader/S_effect_font.fx"
 			|| aFilePath == "Data/Resource/Shader/S_effect_sprite.fx"
 			|| aFilePath == "Data/Resource/Shader/S_effect_debug.fx"
