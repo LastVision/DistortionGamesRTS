06cf0186269e8c55f93c91dc0adaed166179b628
linus.ljungman.sp14@thegameassembly.com <linus.ljungman.sp14@thegameassembly.com>
2015-12-08 16:50:43 +0100

    added class

---
+++ Solution/Game/ConsoleBackspace.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "ConsoleBackspace.h"
+
+
+ConsoleBackspace::ConsoleBackspace()
+{
+}
+
+
+ConsoleBackspace::~ConsoleBackspace()
+{
+}
---
+++ Solution/Game/ConsoleBackspace.h
@@ -0,0 +1,8 @@
+#pragma once
+class ConsoleBackspace
+{
+public:
+	ConsoleBackspace();
+	~ConsoleBackspace();
+};
+
