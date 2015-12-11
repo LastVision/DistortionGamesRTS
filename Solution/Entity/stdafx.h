3aef19a88f4f8b9e9f1a566cefee7da076d91f4e
LastVision <daniel.carlsson92@gmail.com>
2015-12-11 10:33:02 +0100

    Added some includes in Entity's Precompiled header to reduce build speed also removed inputwrapper from entity project which was unused, and disabled threaded loading because it crashed in exes

--- Solution/Entity/stdafx.h
+++ Solution/Entity/stdafx.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#include "Component.h"
 #include <DebugDrawer.h>
+#include <DL_Debug.h>
+#include "Entity.h"
+#include "EntityData.h"
+#include "EntityEnum.h"
 #include <GameConstants.h>
+#include <GrowingArray.h>
+#include <Matrix.h>
 #include <MemoryMacros.h>
+#include <StaticArray.h>
+#include <string>
+#include <unordered_map>
+#include <Vector.h>
+#include <XMLReader.h>
