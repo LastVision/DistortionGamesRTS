e2c50fde5c529ceac6bc07062ef91236cbc0b726
LastVision <daniel.carlsson92@gmail.com>
2015-12-15 12:11:25 +0100

    Removed ParticleComponent from Enity also removed ParticleSystem dependency from Entity

--- Solution/Game/EmitterManager.cpp
+++ Solution/Game/EmitterManager.cpp
@@ -4,7 +4,6 @@
 #include "EmitterManager.h"
 #include <ParticleDataContainer.h>
 #include <Entity.h>
-#include <ParticleComponent.h>
 #include <ParticleEmitterInstance.h>
 #include <PostMaster.h>
 #include <XMLReader.h>
