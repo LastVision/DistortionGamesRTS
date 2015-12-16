8a9b0aba06cb69c121003b5f5ed4ded541a055be
D3c0de <ls12_ac-web@hotmail.com>
2015-12-16 17:14:07 +0100

    Reduced the amount of text being rendered from history to the console.

--- Solution/Game/ConsoleState.cpp
+++ Solution/Game/ConsoleState.cpp
@@ -214,7 +214,7 @@ void ConsoleState::Render()
 	CU::Vector2<float> position = myLowerLeftCorner * 1.1f;
 	position.y += 30.f;
 
-	for (int i = history.Size() - 1; i >= 0; --i)
+	for (int i = history.Size() - 1; i >= history.Size()-23; --i)
 	{
 		position.y += 30.f;
 		history[i]->myRenderText->SetPosition(position);
