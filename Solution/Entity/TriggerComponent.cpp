960725df25617a83725553857dc0b9682465e16d
fabs <fabian.ahlund.sp14@thegameassembly.com>
2015-12-17 09:50:06 +0100

    dead units don't capture points

--- Solution/Entity/TriggerComponent.cpp
+++ Solution/Entity/TriggerComponent.cpp
@@ -42,7 +42,8 @@ void TriggerComponent::CheckUnitsForRemove(CU::GrowingArray<Entity*>& someUnits)
 	{
 		Entity* current = someUnits[i];
 		if (CU::Intersection::CircleVsCircle(myEntity.GetPosition(), myRadius
-			, current->GetPosition(), current->GetComponent<CollisionComponent>()->GetRadius()) == false)
+			, current->GetPosition(), current->GetComponent<CollisionComponent>()->GetRadius()) == false
+			|| (someUnits[i]->GetState() == eEntityState::DYING && someUnits[i]->GetAlive() == false))
 		{
 			PostMaster::GetInstance()->SendMessage(
 				TriggerMessage(&myEntity, current, TriggerMessage::eTriggerType::EXIT));
