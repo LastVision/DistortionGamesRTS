804cb5f676f8aea725577ad3ea59d34ee1909878
D3c0de <ls12_ac-web@hotmail.com>
2015-12-18 11:33:54 +0100

    Added GrenadeComponent

---
+++ Solution/Entity/GrenadeComponent.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "GrenadeComponent.h"
+
+
+GrenadeComponent::GrenadeComponent(Entity& anEntity)
+	: Component(anEntity)
+{
+}
+
+
+GrenadeComponent::~GrenadeComponent()
+{
+}
---
+++ Solution/Entity/GrenadeComponent.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Component.h"
+class GrenadeComponent : public Component
+{
+public:
+	GrenadeComponent(Entity& anEntity);
+	~GrenadeComponent();
+};
+
