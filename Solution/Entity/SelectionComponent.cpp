513c80537318d9e724823606e398a8ea7f7ad70c
fabs <fabian.ahlund.sp14@thegameassembly.com>
2016-01-04 11:24:04 +0100

    added selectiondata

--- Solution/Entity/SelectionComponent.cpp
+++ Solution/Entity/SelectionComponent.cpp
@@ -1,12 +1,32 @@
 #include "stdafx.h"
+
+#include "Instance.h"
 #include "SelectionComponent.h"
 
 
-SelectionComponent::SelectionComponent()
+SelectionComponent::SelectionComponent(Entity& aEntity, SelectionComponentData& aComponentData)
+	: Component(aEntity)
+	, myHover(nullptr)
 {
+	Prism::ModelProxy* model = Prism::ModelLoader::GetInstance()->LoadModel(aComponentData.myModelPath
+		, aComponentData.myEffectPath);
+
+	myInstance = new Prism::Instance(*model, myEntity.myOrientation, myEntity.GetOctreeType(), myCullingRadius);
 }
 
 
 SelectionComponent::~SelectionComponent()
 {
+}
+
+void SelectionComponent::Update(float aDeltaTime)
+{
+	if (myEntity.IsHovered() == true)
+	{
+
+	}
+	if (myEntity.IsSelected() == true)
+	{
+
+	}
 }
--- Solution/Entity/SelectionComponent.h
+++ Solution/Entity/SelectionComponent.h
@@ -1,8 +1,33 @@
 #pragma once
-class SelectionComponent
+
+namespace Prism
+{
+	class Instance;
+}
+
+struct SelectionComponentData;
+
+class SelectionComponent : public Component
 {
 public:
-	SelectionComponent();
+	SelectionComponent(Entity& aEntity, SelectionComponentData& aComponentData);
 	~SelectionComponent();
+
+	void Update(float aDeltaTime) override;
+
+	static eComponentType GetTypeStatic();
+	eComponentType GetType() override;
+
+private:
+	Prism::Instance* myHover;
 };
 
+inline eComponentType SelectionComponent::GetTypeStatic()
+{
+	return eComponentType::SELECTION;
+}
+
+inline eComponentType SelectionComponent::GetType()
+{
+	return GetTypeStatic();
+}
