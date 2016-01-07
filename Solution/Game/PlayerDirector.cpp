4294ca1fd15b31b5e02f5b22aa52f035a469e3e6
fabs <fabian.ahlund.sp14@thegameassembly.com>
2016-01-07 12:02:16 +0100

    if multiple units are selected, only one sound plays

--- Solution/Game/PlayerDirector.cpp
+++ Solution/Game/PlayerDirector.cpp
@@ -270,10 +270,10 @@ void PlayerDirector::ReceiveMessage(const TimeMultiplierMessage& aMessage)
 void PlayerDirector::ReceiveMessage(const MinimapMoveMessage& aMessage)
 {
 	CU::Vector2<float> position = aMessage.myPosition * 255.f;
-
+	bool myHasPlayedSound = false;
 	for (int i = 0; i < mySelectedUnits.Size(); i++)
 	{
-		mySelectedUnits[i]->GetComponent<ControllerComponent>()->MoveTo({ position.x, 0.f, position.y }, true);
+		mySelectedUnits[i]->GetComponent<ControllerComponent>()->MoveTo({ position.x, 0.f, position.y }, true, myHasPlayedSound);
 	}
 }
 
@@ -585,7 +585,7 @@ void PlayerDirector::UpdateMouseInteraction(const Prism::Camera& aCamera)
 	bool hasDoneAction = false;
 
 	CU::Intersection::LineSegment3D line(aCamera.GetOrientation().GetPos(), firstTargetPos);
-
+	bool myHasPlayedSound = false;
 	for (int i = 0; i < myActiveUnits.Size(); ++i)
 	{
 		SelectOrHoverEntity(myActiveUnits[i], hasSelected, hasHovered, line);
@@ -596,27 +596,27 @@ void PlayerDirector::UpdateMouseInteraction(const Prism::Camera& aCamera)
 			if ((mySelectedAction == eSelectedAction::ATTACK_TAGRET && hoveredEnemy != nullptr && myLeftMouseUp == true)
 				|| (hoveredEnemy != nullptr && myRightClicked == true))
 			{
-				controller->AttackTarget(hoveredEnemy, !myShiftPressed);
+				controller->AttackTarget(hoveredEnemy, !myShiftPressed, myHasPlayedSound);
 				hasDoneAction = true;
 			}
 			else if (mySelectedAction == eSelectedAction::ATTACK_MOVE && myLeftMouseUp == true)
 			{
-				controller->AttackMove(firstTargetPos, !myShiftPressed);
+				controller->AttackMove(firstTargetPos, !myShiftPressed, myHasPlayedSound);
 				hasDoneAction = true;
 			}
 			else if ((mySelectedAction == eSelectedAction::MOVE && myLeftMouseUp) || myRightClicked)
 			{
-				controller->MoveTo(firstTargetPos, !myShiftPressed);
+				controller->MoveTo(firstTargetPos, !myShiftPressed, myHasPlayedSound);
 				hasDoneAction = true;
 			}
 			else if (mySelectedAction == eSelectedAction::STOP)
 			{
-				controller->Stop();
+				controller->Stop(myHasPlayedSound);
 				hasDoneAction = true;
 			}
 			else if (mySelectedAction == eSelectedAction::HOLD_POSITION)
 			{
-				controller->HoldPosition();
+				controller->HoldPosition(myHasPlayedSound);
 				hasDoneAction = true;
 			}
 		}
@@ -719,9 +719,10 @@ void PlayerDirector::Enrage()
 
 void PlayerDirector::AttackMoveSelectedUnits(const CU::Vector2<float>& aPosition)
 {
+	bool myHasPlayedSound = false;
 	for (int i = 0; i < mySelectedUnits.Size(); i++)
 	{
-		mySelectedUnits[i]->GetComponent<ControllerComponent>()->AttackMove({ aPosition.x, 0.f, aPosition.y }, !myShiftPressed);
+		mySelectedUnits[i]->GetComponent<ControllerComponent>()->AttackMove({ aPosition.x, 0.f, aPosition.y }, !myShiftPressed, myHasPlayedSound);
 	}
 	mySelectedAction = eSelectedAction::NONE;
 }
