27d03ec7e454cd6e43eef4529585c4f014f02545
fabs <fabian.ahlund.sp14@thegameassembly.com>
2016-01-19 17:19:27 +0100

    added new class

---
+++ Solution/Engine/SelectionCircle.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "SelectionCircle.h"
+
+
+SelectionCircle::SelectionCircle()
+{
+}
+
+
+SelectionCircle::~SelectionCircle()
+{
+}
---
+++ Solution/Engine/SelectionCircle.h
@@ -0,0 +1,8 @@
+#pragma once
+class SelectionCircle
+{
+public:
+	SelectionCircle();
+	~SelectionCircle();
+};
+
