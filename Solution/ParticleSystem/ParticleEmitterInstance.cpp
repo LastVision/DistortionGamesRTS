8448e1deb8774312aa97e155d3acfa10c7343c7c
D3c0de <ls12_ac-web@hotmail.com>
2016-01-22 09:45:36 +0100

    Revert "Merge"
    This reverts commit 85b8a84b56a1233006d554d533bf7cab32d1135c.

--- Solution/ParticleSystem/ParticleEmitterInstance.cpp
+++ Solution/ParticleSystem/ParticleEmitterInstance.cpp
@@ -14,7 +14,6 @@ namespace Prism
 	ParticleEmitterInstance::ParticleEmitterInstance(ParticleEmitterData* someData, bool anAllowManyParticles)
 		: myVertexWrapper(nullptr)
 		, myIsActive(false)
-		, myUseAlphaDelta(false)
 		, myEmissionTime(0)
 		, myEmitterLife(0)
 		, myParticleIndex(0)
