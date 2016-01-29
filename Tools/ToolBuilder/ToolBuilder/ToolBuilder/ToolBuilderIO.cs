8bff7a0b9e58a29e51be5a17436a9d6851d9bfd4
LastVision <daniel.carlsson92@gmail.com>
2016-01-29 16:59:35 +0100

    Tool Builder (WIP)

---
+++ Tools/ToolBuilder/ToolBuilder/ToolBuilder/ToolBuilderIO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolBuilder.ToolBuilder
+{
+    enum eIOType
+    {
+        IN,
+        OUT,
+        INOUT
+    };
+
+    struct ToolBuilderIO
+    {
+        public eIOType myIOType;
+        public String myIOComponentType;
+        public String myIOElementName;
+        public String[] myIOAttribute;
+    }
+}
