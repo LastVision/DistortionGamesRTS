9cc998f7800850711d84370720bce5647b217af1
apasitteribuske <malin.sandgren.sp14@thegameassembly.com>
2016-02-09 16:14:29 +0100

    Fixed units hovering incorrectly

--- Solution/Game/PlayerDirector.cpp
+++ Solution/Game/PlayerDirector.cpp
@@ -1161,7 +1161,7 @@ void PlayerDirector::SelectOrHoverEntity(Entity* aEntity, bool &aSelected, bool 
 	aEntity->SetHovered(false);
 
 	bool unitCollided = false;
-	if (myRenderDragSelection == true)
+	if (myRenderDragSelection == true && (myLeftMouseUp == true || myLeftMousePressed == true))
 	{
 		CU::Vector2<float> position1(myDragSelectionPositions[0].x, myDragSelectionPositions[0].z);
 		CU::Vector2<float> position2(myDragSelectionPositions[2].x, myDragSelectionPositions[2].z);
