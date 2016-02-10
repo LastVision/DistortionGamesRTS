4c831b69fc366b8ceb4a8996fa6cabfbc0e9de9a
fabs <fabian.ahlund.sp14@thegameassembly.com>
2016-02-10 14:48:47 +0100

    totem stops its sound when level exits

--- Solution/Game/InGameState.cpp
+++ Solution/Game/InGameState.cpp
@@ -369,6 +369,7 @@ void InGameState::RestartLevel()
 {
 	Prism::Audio::AudioInterface::GetInstance()->PostEvent("FadeForWinLoose", 0);
 	Prism::Audio::AudioInterface::GetInstance()->PostEvent("PlayLoose", 0);
+	Prism::Audio::AudioInterface::GetInstance()->PostEvent("Stop_Totem", 0);
 	//bool runtime = //Prism::MemoryTracker::GetInstance()->GetRunTime();
 	//Prism::MemoryTracker::GetInstance()->SetRunTime(false);
 	GameStateMessage* newEvent = new GameStateMessage(eGameState::RELOAD_LEVEL);
@@ -380,6 +381,7 @@ void InGameState::CompleteLevel()
 {
 	Prism::Audio::AudioInterface::GetInstance()->PostEvent("FadeForWinLoose", 0);
 	Prism::Audio::AudioInterface::GetInstance()->PostEvent("PlayWin", 0);
+	Prism::Audio::AudioInterface::GetInstance()->PostEvent("Stop_Totem", 0);
 	//bool runtime = //Prism::MemoryTracker::GetInstance()->GetRunTime();
 	//Prism::MemoryTracker::GetInstance()->SetRunTime(false);
 	GameStateMessage* newEvent = new GameStateMessage(eGameState::LOAD_NEXT_LEVEL);
@@ -391,6 +393,7 @@ void InGameState::CompleteGame()
 {
 	Prism::Audio::AudioInterface::GetInstance()->PostEvent("FadeForWinLoose", 0);
 	Prism::Audio::AudioInterface::GetInstance()->PostEvent("PlayWin", 0);
+	Prism::Audio::AudioInterface::GetInstance()->PostEvent("Stop_Totem", 0);
 	//Fadea ut levelmusiken
 	//Spela win 
 	ShowMessage("Data/Resource/Texture/Menu/T_background_completed_game.dds", { 1024, 1024 }, "Press [space] to continue.");
